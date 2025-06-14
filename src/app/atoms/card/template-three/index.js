7fd5ffda61d7bc6c0a4c2919bdc3636851458bc8
syed-zunair-gillani <dev.zunaair@gmail.com>
2025-06-14 19:37:40 +0500

    update

--- src/app/atoms/card/template-three/index.js
+++ src/app/atoms/card/template-three/index.js
@@ -9,18 +9,24 @@ const TemplateThreeCard = ({ image, title, intro, link, category, time, priority
       flex: 1
     }}>
       <Link href={link} className={styles.contentTxt}>
-      <Image
-         src={image}
-         alt={title}
-         width={500} 
-         height={300}
-      />
+        <Image
+          src={image}
+          alt={title}
+          width={500}
+          height={300}
+          priority // Ensures LCP image loads early
+          placeholder="blur" // Optional: Use blur-up for better UX
+          blurDataURL="/placeholder.jpg" // Replace with actual low-quality image placeholder
+          sizes="(max-width: 768px) 100vw, 500px" // Responsive loading
+          style={{ width: '100%', height: 'auto' }} // Prevent layout shift
+        />
+
         <h2 className="post-card-title">{title}</h2>
         {intro && <p className="post-card-intro">{intro}</p>}
       </Link>
       <div className={styles.cardFooter}>
         <span>{time} Min Lesezeit | </span>
-        {category &&  <span className={styles.category}>{category}</span>}
+        {category && <span className={styles.category}>{category}</span>}
       </div>
     </div>
   );
